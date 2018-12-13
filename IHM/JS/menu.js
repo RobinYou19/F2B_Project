1e48546fbe63e52c8605c2fe59313dcc3a857d17
MrChapelle <robin.you@telecom-bretagne.eu>
2018-12-13 17:45:15 +0100

    add IHM section for IHM development

---
+++ IHM/JS/menu.js
@@ -0,0 +1,10 @@
+var myElement = window.document;
+
+// create a simple instance
+// by default, it only adds horizontal recognizers
+var mc = new Hammer(myElement);
+
+// listen to events...
+mc.on("panleft panright tap press", function(ev) {
+    console.log(ev.type +" gesture detected.");
+});
