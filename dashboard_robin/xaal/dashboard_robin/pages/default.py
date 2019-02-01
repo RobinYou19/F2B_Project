7370a11e368cef768534ab5a37df45b9e816ebf0
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-02-01 15:29:49 +0100

    changing xaal.dashboard to xaal.dashboard_robin

--- dashboard_robin/xaal/dashboard_robin/pages/default.py
+++ dashboard_robin/xaal/dashboard_robin/pages/default.py
@@ -1,7 +1,7 @@
 from bottle import route
 from bottle import mako_view as view
 
-from xaal.dashboard.core import xaal_core
+from xaal.dashboard_robin.core import xaal_core
 #from core import xaal_core
 
 
