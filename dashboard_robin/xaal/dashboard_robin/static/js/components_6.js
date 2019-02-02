9db0b2ecc7476f075c4da0f81aba48e977306cb5
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-02-02 16:55:25 +0100

    components structured with inheritance

--- dashboard_robin/xaal/dashboard_robin/static/js/components_6.js
+++ dashboard_robin/xaal/dashboard_robin/static/js/components_6.js
@@ -84,7 +84,7 @@ class valueTitle extends HTMLElement
     }
     else
     {
-      this.title = "Unfound Title";
+      this.title = "No Title";
     }
     if (this.hasAttribute('value'))
     {
@@ -92,7 +92,7 @@ class valueTitle extends HTMLElement
     }
     else
     {
-      this.value = "Unfound Value";
+      this.value = "No Value";
     }
 
     if (!this.shadowRoot)
@@ -315,6 +315,120 @@ class OnOffButtons extends HTMLElement
 
 customElements.define('onoff-buttons', OnOffButtons);
 
+class BasicDevice extends HTMLElement
+{
+  constructor()
+  {
+    super();
+  }
+
+  connectedCallback()
+  {
+    if (this.hasAttribute('address'))
+    {
+      this.address = this.getAttribute('address');
+    }
+    else
+    {
+      this.address = "";
+    }
+    if (this.hasAttribute('src'))
+    {
+      this.src = this.getAttribute('src');
+    }
+    else
+    {
+      this.src = "/static/imgs/default-image.jpeg";
+    }
+    if (this.hasAttribute('title'))
+    {
+      this.title = this.getAttribute('title');
+    }
+    else
+    {
+      this.title = 'BasicDevice';
+    }
+    if (this.hasAttribute('height'))
+    {
+      this.height = this.getAttribute('height');
+    }
+    else
+    {
+      this.height = '50';
+    }
+    if (this.hasAttribute('width'))
+    {
+      this.width = this.getAttribute('width');
+    }
+    else
+    {
+      this.width = '50';
+    }
+    if (this.hasAttribute('value'))
+    {
+      this.value = this.getAttribute('value');
+    }
+    else
+    {
+      this.value = 'NoValue';
+    }
+    if (this.hasAttribute('class'))
+    {
+      this.class = this.getAttribute('class');
+    }
+    else
+    {
+      this.class = 'box';
+    }
+
+    if (!this.shadowRoot)
+    {
+      var shadow  = this.attachShadow({mode : 'open'});
+      var content = document.createElement('div');
+      var style   = document.createElement('style');
+      var image   = document.createElement('image-basic');
+
+      if (this.value != 'NoValue')
+      {
+        var title = document.createElement('title-value');
+        title.setAttribute('title', this.title);
+        title.setAttribute('value', this.value);
+      }
+      else
+      {
+        var title = document.createElement('title-basic');
+        title.setAttribute('title', this.title);
+      }
+
+      var div_id = "basic_device" + this.address ;
+      content.setAttribute('div', div_id);
+      content.setAttribute('class', this.class);
+
+      image.setAttribute('src', this.src);
+      image.setAttribute('height', this.height);
+      image.setAttribute('width', this.width);
+      image.setAttribute('address', this.address);
+
+      style.textContent = `
+      .box 
+      {
+        background-color: rgba(255, 255, 255, .5);
+        border-style: groove;
+        border-radius: 5px;
+        height: 150%;
+        margin: 5%;
+      }
+      `;
+
+      shadow.appendChild(style);
+      shadow.appendChild(content);
+      content.appendChild(title);
+      content.appendChild(image);
+    }
+  }
+}
+
+customElements.define('basic-device', BasicDevice);
 
 class OnOffDevice extends HTMLElement 
 {
@@ -373,14 +487,21 @@ class OnOffDevice extends HTMLElement
     {
       this.width = '50';
     }
+    if (this.hasAttribute('value'))
+    {
+      this.value = this.getAttribute('value');
+    }
+    else
+    {
+      this.value = "NoValue";
+    }
 
     if (!this.shadowRoot)
     {
       var shadow  = this.attachShadow({mode : 'open'});
       var content = document.createElement('div');
       var style   = document.createElement('style');
-      var title   = document.createElement('title-basic');
-      var image   = document.createElement('image-basic');
+      var bs_dev  = document.createElement('basic-device');
       var buttons = document.createElement('onoff-buttons');
       var endline = document.createElement('br');
 
@@ -395,12 +516,13 @@ class OnOffDevice extends HTMLElement
         content.setAttribute('class', 'box box-off');
       }
 
-      title.setAttribute('title', this.title);
-
-      image.setAttribute('src', this.src);
-      image.setAttribute('height', this.height);
-      image.setAttribute('width', this.width);
-      image.setAttribute('address', this.address);
+      bs_dev.setAttribute('title', this.title);
+      bs_dev.setAttribute('src', this.src);
+      bs_dev.setAttribute('height', this.height);
+      bs_dev.setAttribute('width', this.width);
+      bs_dev.setAttribute('address', this.address);
+      bs_dev.setAttribute('value', this.value);
+      bs_dev.setAttribute('class', 'inside');
 
       buttons.setAttribute('address', this.address);
 
@@ -426,8 +548,7 @@ class OnOffDevice extends HTMLElement
 
       shadow.appendChild(style);
       shadow.appendChild(content);
-      content.appendChild(title);
-      content.appendChild(image);
+      content.appendChild(bs_dev);
       content.appendChild(endline);
       content.appendChild(buttons);
     }
@@ -435,110 +556,3 @@ class OnOffDevice extends HTMLElement
 }
 
 customElements.define('onoff-device', OnOffDevice);
-
-class BasicDevice extends HTMLElement
-{
-  constructor()
-  {
-    super();
-  }
-
-  connectedCallback()
-  {
-    if (this.hasAttribute('address'))
-    {
-      this.address = this.getAttribute('address');
-    }
-    else
-    {
-      this.address = "";
-    }
-    if (this.hasAttribute('src'))
-    {
-      this.src = this.getAttribute('src');
-    }
-    else
-    {
-      this.src = "/static/imgs/default-image.jpeg";
-    }
-    if (this.hasAttribute('title'))
-    {
-      this.title = this.getAttribute('title');
-    }
-    else
-    {
-      this.title = 'BasicDevice';
-    }
-    if (this.hasAttribute('height'))
-    {
-      this.height = this.getAttribute('height');
-    }
-    else
-    {
-      this.height = '50';
-    }
-    if (this.hasAttribute('width'))
-    {
-      this.width = this.getAttribute('width');
-    }
-    else
-    {
-      this.width = '50';
-    }
-    if (this.hasAttribute('value'))
-    {
-      this.value = this.getAttribute('value');
-    }
-    else
-    {
-      this.value = "NoValue";
-    }
-
-    if (!this.shadowRoot)
-    {
-      var shadow  = this.attachShadow({mode : 'open'});
-      var content = document.createElement('div');
-      var style   = document.createElement('style');
-      var image   = document.createElement('image-basic');
-
-      if (this.value != "NoValue")
-      {
-        var title = document.createElement('title-value');
-        title.setAttribute('title', this.title);
-        title.setAttribute('value', this.value);
-      }
-      else
-      {
-        var title = document.createElement('title-basic');
-        title.setAttribute('title', this.title);
-      }
-
-      var div_id = "basic_device" + this.address ;
-      content.setAttribute('div', div_id);
-      content.setAttribute('class', 'box');
-
-      image.setAttribute('src', this.src);
-      image.setAttribute('height', this.height);
-      image.setAttribute('width', this.width);
-      image.setAttribute('address', this.address);
-
-      style.textContent = `
-      .box 
-      {
-        background-color: rgba(255, 255, 255, .5);
-        border-style: groove;
-        border-radius: 5px;
-        height: 150%;
-        margin: 5%;
-      }
-      `;
-
-      shadow.appendChild(style);
-      shadow.appendChild(content);
-      content.appendChild(title);
-      content.appendChild(image);
-    }
-  }
-}
-
-customElements.define('basic-device', BasicDevice);
