bc2a562581451f5319b4758514cc541769510c23
RobinYou19 <robin.you@telecom-bretagne.eu>
2019-02-26 00:26:43 +0100

    WIP on config file and base.py files : modules done, need to do favorites and scenarios

--- dashboard_robin/xaal/dashboard_robin/pages/base.py
+++ dashboard_robin/xaal/dashboard_robin/pages/base.py
@@ -12,19 +12,6 @@ config_path = this_directory + filename
 with open(config_path) as f:
   data = json.load(f)
 
-double_lamps        = [('lamp_entree' , 'lamp_couloir'), 
-                       ('lamp_salon'  , 'lamp_salle'),
-                       ('lamp_cuisine', 'lamp_sdb')]
-
-shutters            = [('Volet_Cuisine','2fe70f46-3ece-44d1-af34-2d82e10fb854'), 
-                       ('Volet_SDB'    ,'e4b05165-be5d-46d5-acd0-4da7be1158ed')]
-
-double_thermometers = [('temp_owm', 'temp_bureau')]
-double_hygrometers  = [('rh_owm'  , 'rh_bureau'  )]
-
-wall_plug           = ["5e50a1ed-5290-4cdb-b00f-1f968eee4401",
-                       "5e50a1ed-5290-4cdb-b00f-1f968eee4402"]
-
 #################################################################
 #@ MENU PAGES
 
@@ -36,21 +23,18 @@ def menu():
 @route('/house')
 @view('house.mako')
 def get_devices_house():
-    r = {"title" : "House"}
+    r = {'title' : 'House'}
     devs = xaal_core.monitor.devices
-    r.update({"devs" : devs})
-    r.update({"double_lamps" : double_lamps})
-    r.update({"shutters" : shutters})
-    r.update({"double_thermometers" : double_thermometers})
-    r.update({"double_hygrometers" : double_hygrometers})
-    r.update({"wall_plug" : wall_plug})
+    r.update({'devs' : devs})
     r.update({'house' : data['pages']['house']})
     return r
 
 @route('/modules')
 @view('modules.mako')
 def modules():
-    return {'title':'Modules'}
+    r = {'title':'Modules'}
+    r.update({'modules' : data['pages']['modules']})
+    return r
 
 @route('/scenarios')
 @view('scenarios.mako')
@@ -109,7 +93,7 @@ def save_metadata(addr):
 def account():
     r = {"title" : "Account"}
     devs = xaal_core.monitor.devices
-    r.update({"devs" : devs})
+    r.update({'devs' : devs})
     return r
 
 
@@ -119,12 +103,16 @@ def account():
 @route('/type')
 @view('type.mako')
 def type():
-    return {'title':'Type'}
+    r =  {'title':'Type'}
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
+    return r
 
 @route('/localisation')
 @view('localisation.mako')
 def localisation():
-    return {'title':'Localisation'}
+    r = {'title':'Localisation'}
+    r.update({'objects' : data['pages']['modules'][1]['objects']})
+    return r
 
 
 #################################################################
@@ -136,6 +124,7 @@ def get_devices_barometers():
     r = {"title" : "devices list"}
     devs = xaal_core.monitor.devices
     r.update({"devs" : devs})
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
     return r
 
 @route('/gateways')
@@ -144,6 +133,7 @@ def get_devices_gateways():
     r = {"title" : "devices list"}
     devs = xaal_core.monitor.devices
     r.update({"devs" : devs})
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
     return r
 
 @route('/hmis')
@@ -152,6 +142,7 @@ def get_devices_hmis():
     r = {"title" : "devices list"}
     devs = xaal_core.monitor.devices
     r.update({"devs" : devs})
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
     return r
 
 @route('/hygrometers')
@@ -160,6 +151,7 @@ def get_devices_hygrometers():
     r = {"title" : "devices list"}
     devs = xaal_core.monitor.devices
     r.update({"devs" : devs})
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
     return r
 
 @route('/lights')
@@ -168,6 +160,7 @@ def get_devices_lights():
     r = {"title" : "devices list"}
     devs = xaal_core.monitor.devices
     r.update({"devs" : devs})
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
     return r
 
 @route('/power_relays')
@@ -176,6 +169,7 @@ def get_devices_power_relays():
     r = {"title" : "devices list"}
     devs = xaal_core.monitor.devices
     r.update({"devs" : devs})
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
     return r
 
 @route('/thermometers')
@@ -184,6 +178,7 @@ def get_devices_thermometers():
     r = {"title" : "devices list"}
     devs = xaal_core.monitor.devices
     r.update({"devs" : devs})
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
     return r
 
 @route('/windgauges')
@@ -192,6 +187,7 @@ def get_devices_windgauges():
     r = {"title" : "devices list"}
     devs = xaal_core.monitor.devices
     r.update({"devs" : devs})
+    r.update({'objects' : data['pages']['modules'][0]['objects']})
     return r
 
 #################################################################
@@ -217,8 +213,7 @@ def get_devices_bureau():
   r = {"title" : "devices bureau list"}
   devs = xaal_core.monitor.devices
   r.update({"devs" : devs})
-  r.update({"thermometer_bureau" : double_thermometers[0][1]})
-  r.update({"hygrometer_bureau" : double_hygrometers[0][1]})
+  r.update({"list" : data['pages']['modules'][1]['objects'][0]['list']})
   return r
 
 @route('/couloir')
@@ -227,7 +222,7 @@ def get_devices_couloir():
   r = {"title" : "devices couloir list"}
   devs = xaal_core.monitor.devices
   r.update({"devs" : devs})
-  r.update({"lamp_couloir" : double_lamps[0][1]})
+  r.update({"list" : data['pages']['modules'][1]['objects'][1]['list']})
   return r
 
 @route('/cuisine')
@@ -236,8 +231,7 @@ def get_devices_cuisine():
   r = {"title" : "devices cuisine list"}
   devs = xaal_core.monitor.devices
   r.update({"devs" : devs})
-  r.update({"lamp_cuisine" : double_lamps[2][0]})
-  r.update({"shutter_cuisine" : shutters[0]})
+  r.update({"list" : data['pages']['modules'][1]['objects'][2]['list']})
   return r
 
 @route('/entree')
@@ -246,7 +240,7 @@ def get_devices_entree():
   r = {"title" : "devices entree list"}
   devs = xaal_core.monitor.devices
   r.update({"devs" : devs})
-  r.update({"lamp_entree" : double_lamps[0][0]})
+  r.update({"list" : data['pages']['modules'][1]['objects'][3]['list']})
   return r
 
 @route('/salle')
@@ -255,7 +249,7 @@ def get_devices_salle():
   r = {"title" : "devices salle list"}
   devs = xaal_core.monitor.devices
   r.update({"devs" : devs})
-  r.update({"lamp_salle" : double_lamps[1][1]})
+  r.update({"list" : data['pages']['modules'][1]['objects'][4]['list']})
   return r
 
 @route('/salon')
@@ -264,7 +258,7 @@ def get_devices_salon():
   r = {"title" : "devices salon list"}
   devs = xaal_core.monitor.devices
   r.update({"devs" : devs})
-  r.update({"lamp_salon" : double_lamps[1][0]})
+  r.update({"list" : data['pages']['modules'][1]['objects'][5]['list']})
   return r
 
 @route('/sdb')
@@ -273,8 +267,7 @@ def get_devices_sdb():
   r = {"title" : "devices salle de bain list"}
   devs = xaal_core.monitor.devices
   r.update({"devs" : devs})
-  r.update({"lamp_sdb" : double_lamps[2][1]})
-  r.update({"shutter_sdb" : shutters[1]})
+  r.update({"list" : data['pages']['modules'][1]['objects'][6]['list']})
   return r
 
 
